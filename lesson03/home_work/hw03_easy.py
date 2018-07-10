d2f11b1ccb87b70e249cdb561262e3a2400cc6b6
NikStrelchenko <nickita.strelchenko@yandex.ru>
2018-07-10 10:10:16 +0300

    изи

--- lesson03/home_work/hw03_easy.py
+++ lesson03/home_work/hw03_easy.py
@@ -11,15 +11,17 @@
 #print(my_round(2.1234567, 5))
 #print(my_round(2.1999967, 5))
 #print(my_round(2.9999967, 5))
-#def my_round(number, ndigits):
-#    number = number * (10 ** ndigits)
-#    if float(number) - int(number) > 0.5:
-#         number = number // 1 + 1
-#    else:
-#         number = number // 1
-#    return number / (10 ** ndigits)
-#print(my_round(2.1234567, 1))
 
+def my_round(number, ndigits):
+    number = number * (10 ** ndigits)
+    if float(number) - int(number) > 0.5:
+         number = number // 1 + 1
+    else:
+         number = number // 1
+    return number / (10 ** ndigits)
+print(my_round(2.1234567, 5))
+print(my_round(2.1999967, 5))
+print(my_round(2.9999967, 5))
 
 
 # Задание-2:
@@ -35,44 +37,20 @@
 #print(lucky_ticket(123006))
 #print(lucky_ticket(12321))
 #print(lucky_ticket(436751))
-#def lucky_ticket(ticket_number):
-#    lucky_ticket = lambda x :(lambda x : 'yes' if sum(x[:3]) == sum(x[3:]) else 'no')(map(int, list(str(x)))) 
-#    print(lucky_ticket(129567))
 
-
-#def lucky_ticket(ticket_number):
- #   for i in range (ticket_number):
- #       if sum(i[:3])== sum(i[3:]):
-#               print ("Lucky!!")
-#        else:
-#            print ("Bad")
- #           return(x)
-#ticket_number = int(input())
-#print (x)
-
-
-
-#def lucky_ticket(ticket_number):
- #   if (len(str(ticket_number))!= 6) or (type(ticket_number) is not int):
-#        return 'Некорректный номер билета'
-#    else:
-#        ticket_number1 = list(str(ticket_number))
-#        for ticket in ticket_number1:
-#        if ticket[0] + ticket[1] + ticket[2] == ticket[3] + ticket[4] + ticket[5]:
-#            return 'Билет %s счастливый' %ticket_number
-#        else:
-#            return 'Билет %s несчастливый' %ticket_number
-
-#number = int(input('Введите номер билета:'))
-#number = 123456
-#print(lucky_ticket(number))
 def lucky_ticket(ticket_number):
-    lucky_ticket = lambda x : (lambda x : "YES" if sum(x[:3]) == sum [3:] else "NO")(list(map(int,list(str(x)))))
-ticket_number = 123456
-print(lucky_ticket)
-
-#FUNCTION = lambda x : (lambda x : 'yes' if sum(x[:3]) == sum(x[3:]) else 'no')(list(map(int, list(str(x)))))
-
-#INPUT=654456
-#RESULT = FUNCTION(INPUT)
-#print(RESULT)
+    if (len(str(ticket_number))!= 6) or (type(ticket_number) is not int):
+        return 'Некорректный номер билета'
+    else:
+        ticket_number1 = list(str(ticket_number))
+        sum1 = int(ticket_number1[0]) + int(ticket_number1[1]) + int(ticket_number1[2])
+        sum2 = int(ticket_number1[3]) + int(ticket_number1[4]) + int(ticket_number1[5])
+        if sum1 == sum2:
+            return 'Билет %s счастливый' %ticket_number
+        else:
+            return 'Билет %s несчастливый' %ticket_number
+#number = 123456
+print(lucky_ticket(123456))
+print(lucky_ticket(123006))
+print(lucky_ticket(12321))
+print(lucky_ticket(436751))
